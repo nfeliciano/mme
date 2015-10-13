eb28f729e929109fe41ff2d84fa975a5b4ed2e3f
Noel Feliciano <feliciano.noel@gmail.com>
2015-10-13 12:43:06 -0700

    Initial Commit

---
+++ Museum Ideas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Museum Ideas
+//
+//  Created by Noel Feliciano on 2015-10-13.
+//  Copyright © 2015 mme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Museum IdeasTests/Museum_IdeasTests.swift
@@ -0,0 +1,36 @@
+//
+//  Museum_IdeasTests.swift
+//  Museum IdeasTests
+//
+//  Created by Noel Feliciano on 2015-10-13.
+//  Copyright © 2015 mme. All rights reserved.
+//
+
+import XCTest
+@testable import Museum_Ideas
+
+class Museum_IdeasTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
