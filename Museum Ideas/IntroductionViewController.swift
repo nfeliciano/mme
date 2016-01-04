39eeee1a1ba6144de81a18f1d4476147b61dd7e3
Noel Feliciano <feliciano.noel@gmail.com>
2016-01-04 15:52:41 -0800

    intro page for now

--- Museum Ideas/IntroductionViewController.swift
+++ Museum Ideas/IntroductionViewController.swift
@@ -16,30 +16,32 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     @IBOutlet weak var cameraButton : UIButton!
     @IBOutlet weak var recordButton : UIButton!
     @IBOutlet weak var continueButton : UIButton!
-    @IBOutlet weak var imageViewTemp : UIImageView!
-    @IBOutlet weak var segmentedControl : UISegmentedControl!
     @IBOutlet weak var scrollView : UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.scrollView.contentSize = CGSizeMake(self.scrollView.frame.size.width*2, self.scrollView.frame.size.height);
+        self.scrollView.delegate = self
         
+        self.playIntroVideo()
     }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-//        self.playVideo()
+        
         let fileManager = NSFileManager.defaultManager()
         let path = getDocumentsDirectory().stringByAppendingPathComponent("myImage.png")
+        let image: UIImage
         if (fileManager.fileExistsAtPath(path)) {
-            let image: UIImage = UIImage(contentsOfFile: path)!
-            self.addImagetoScrollViewAtPage(image, page: 0)
+            image = UIImage(contentsOfFile: path)!
         } else {
-            print("nope")
+            image = UIImage(named: "emptyImage.png")!
         }
+        self.addImagetoScrollViewAtPage(image, page: 0)
         
         let vidPath = getDocumentsDirectory().stringByAppendingPathComponent("myVideo.mp4")
+        let vidImage: UIImage
         if (fileManager.fileExistsAtPath(vidPath)) {
 //            let player = AVPlayer(URL: NSURL(fileURLWithPath: path))
 //            let playerCtrl = AVPlayerViewController()
@@ -47,9 +49,11 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
 //            self.presentViewController(playerCtrl, animated: true) {
 //                playerCtrl.player?.play()
 //            }
+            vidImage = UIImage(named: "videoPlayButton.png")!
         } else {
-            print("nothing here")
+            vidImage = UIImage(named: "emptyVideo.png")!
         }
+        self.addImagetoScrollViewAtPage(vidImage, page: 1)
     }
 
     override func didReceiveMemoryWarning() {
@@ -58,27 +62,36 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     }
     
     func addImagetoScrollViewAtPage(imageToAdd: UIImage, page: Int) {
+        for imageView in self.scrollView.subviews {
+            if (imageView.tag == page) {
+                let replaceView : UIImageView = imageView as! UIImageView
+                replaceView.image = imageToAdd
+                return
+            }
+        }
         let image : UIImageView = UIImageView(frame: CGRectMake(0, 0, 450, 600))
         let frame : CGRect = self.scrollView.frame
         image.center = CGPointMake(frame.size.width/2 + (CGFloat(page) * frame.size.width), frame.size.height/2)
         image.image = imageToAdd
+        image.tag = page;
         self.scrollView .addSubview(image)
     }
     
+    func playIntroVideo() {
+        let path = NSBundle.mainBundle().pathForResource("intro_start", ofType: "MOV")
+        let url = NSURL.fileURLWithPath(path!)
+        let player = AVPlayer(URL: url)
+        let playerCtrl = AVPlayerViewController()
+        playerCtrl.player = player
+        self.presentViewController(playerCtrl, animated: true) {
+            playerCtrl.player?.play()
+        }
+
+    }
+    
     @IBAction func playVideo(sender: UIButton) {
-//        let path = NSBundle.mainBundle().pathForResource("intro_start", ofType: "MOV")
-//        let url = NSURL.fileURLWithPath(path!)
-//        let player = AVPlayer(URL: url)
-//        let playerCtrl = AVPlayerViewController()
-//        playerCtrl.player = player
-//        self.presentViewController(playerCtrl, animated: true) {
-//            playerCtrl.player?.play()
-//        }
-//        
-//        //change this to when the video stops playing
-        self.cameraButton?.hidden = false
-        self.recordButton?.hidden = false
-        self.imageViewTemp?.hidden = false
+//        self.cameraButton?.hidden = false
+//        self.recordButton?.hidden = false
     }
     
     @IBAction func cameraPressed(sender: UIButton) {
@@ -135,6 +148,14 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
                             }
                             let filename = getDocumentsDirectory().stringByAppendingPathComponent("myVideo.mp4")
                             videoData!.writeToFile(filename, atomically: true)
+                            
+                            //add image to scrollview here
+                            for imageView in self.scrollView.subviews {
+                                if (imageView.tag == 1) {
+                                    let replaceView : UIImageView = imageView as! UIImageView
+                                    replaceView.image = UIImage(named: "videoPlayButton.png")!
+                                }
+                            }
                         }
                     }
                     else
@@ -144,10 +165,15 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
                         if let data = UIImagePNGRepresentation(temp) {
                             let filename = getDocumentsDirectory().stringByAppendingPathComponent("myImage.png")
                             data.writeToFile(filename, atomically: true)
+                            
+                            //add image to scrollview here
+                            for imageView in self.scrollView.subviews {
+                                if (imageView.tag == 0) {
+                                    let replaceView : UIImageView = imageView as! UIImageView
+                                    replaceView.image = temp
+                                }
+                            }
                         }
-                        
-                        imageViewTemp.image = temp
-
                     }
                 }
             }
@@ -165,6 +191,16 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     override func performSegueWithIdentifier(identifier: String, sender: AnyObject?) {
         if identifier == "segueToGuide" {
             
+        }
+    }
+    
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        if scrollView.contentOffset.x == 0 {
+            self.cameraButton!.hidden = false;
+            self.recordButton!.hidden = true;
+        } else {
+            self.cameraButton!.hidden = true;
+            self.recordButton!.hidden = false;
         }
     }
     
